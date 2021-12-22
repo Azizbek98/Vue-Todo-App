625ca4fceae6310a34c7c8119c65ba51f7714f9a
Azizbek Samatov <azizbeksamatov98@gmail.com>
2021-12-22 11:42:54 +0500

    feat: adding navigation menu structure to the app component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <nav class="bg-purple-600 text-white">
+    <div class="container mx-auto">
+      <div class="flex items-center">
+        <router-link
+          class="inline-block px-16 py-4 transition-colors hover:bg-purple-500 duration-300"
+          :to="{ name: 'Home' }"
+          >Home</router-link
+        >
+        <router-link
+          class="inline-block px-16 py-4 transition-colors hover:bg-purple-500 duration-300"
+          :to="{}"
+          >Todos</router-link
+        >
+      </div>
+    </div>
+  </nav>
   <router-view />
 </template>
