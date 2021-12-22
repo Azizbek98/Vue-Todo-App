16decf67e440604bd9bb5dfd66141ec213bd2154
Azizbek Samatov <azizbeksamatov98@gmail.com>
2021-12-22 13:45:12 +0500

    feat: adding ui elements of todos component

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -6,7 +6,38 @@
         <div
           class="col-span-6 space-y-4 overflow-y-auto px-1"
           style="height: 500px"
-        ></div>
+        >
+          <div
+            class="p-8 bg-white shadow-md rounded flex items-center justify-between"
+          >
+            <div>
+              <div>First Todo</div>
+              <div class="text-gray-500 text-sm"></div>
+            </div>
+            <div class="space-x-2">
+              <button class="px-2 text-red-600" title="Remove todo">
+                &times;
+              </button>
+              <button class="px-2 text-green-600" title="Mark as complete">
+                &check;
+              </button>
+              <button class="px-2 text-green-600" title="Mark as undone">
+                &#8630;
+              </button>
+            </div>
+          </div>
+          <div
+            class="px-8 py-16 bg-white text-gray-700 shadow-md rounded text-sm"
+          >
+            <p>You don't have tasks todo</p>
+          </div>
+        </div>
+        <div class="col-span-6">
+          <div class="p-8 bg-white shadow-md rounded">
+            <h2 class="text-xl">Add todo</h2>
+            <input type="text" class="p-2 mt-4 border rounded w-full" />
+          </div>
+        </div>
       </div>
     </div>
   </div>
